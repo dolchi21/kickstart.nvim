634210adc96b4a6c14d3b445ddfce8eeff9d56b8
Axel Dolce <axel.dolce@gmail.com>
2024-03-14 21:46:29 -0300

    ignore node_modules

--- init.lua
+++ init.lua
@@ -350,6 +350,11 @@ require('lazy').setup({
         --   },
         -- },
         -- pickers = {}
+        defaults = { 
+          file_ignore_patterns = { 
+            "node_modules" 
+          }
+        },      
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
